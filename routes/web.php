2d0049938c1dee7e58159b34860c789047cf7be7
JonesTheG <4aflarsen@familie.tele.dk>
2022-06-15 15:59:06 +0200

    fixed some text colors

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Route::get('/download', [App\Http\Controllers\HomeController::class, 'download'])->name('download');
-
 Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
 Route::get('/users/update/{user}', [App\Http\Controllers\AdminController::class, 'update'])->name('admin.users.update');
 Route::get('/users/delete/{user}', [App\Http\Controllers\AdminController::class, 'delete'])->name('admin.users.delete');
@@ -37,7 +35,7 @@ Route::group(['prefix' => 'posts', 'middelware' => 'auth'], function () {
     Route::post('/', [App\Http\Controllers\PostController::class, 'store'])->name('posts.store');
     Route::get('/', [App\Http\Controllers\PostController::class, 'index'])->name('posts.list');
     Route::get('/edit/{slug}', [App\Http\Controllers\PostController::class, 'edit'])->name('posts.edit');
-    Route::post('/{slug}', [App\Http\Controllers\PostController::class, 'update'])->name('posts.update');
+    Route::post('/edit/{slug}', [App\Http\Controllers\PostController::class, 'update'])->name('posts.update');
     Route::get('/delete/{slug}', [App\Http\Controllers\PostController::class, 'delete'])->name('posts.delete');
 });
 
